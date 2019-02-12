fc2f3f0ced0d2885f3710ccd3f84f351225b75fe
Nikolay Vinge <vn2756@gmail.com>
2019-02-12 21:23:55 +0400

    First
    Signed-off-by: Nikolay Vinge <vn2756@gmail.com>

---
+++ IISProcessAttach/Command/IISProcessAttachCommand.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.VisualStudio.Shell;
+using System;
+using System.ComponentModel.Design;
+using Task = System.Threading.Tasks.Task;
+
+namespace IISProcessAttach.Command
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class IISProcessAttachCommand
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("9d7c08d3-f6b6-43db-91d5-bde771b8708d");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage _package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IISProcessAttachCommand"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private IISProcessAttachCommand(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this._package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static IISProcessAttachCommand Instance { get; private set; }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private IAsyncServiceProvider ServiceProvider
+        {
+            get { return this._package; }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in IISProcessAttachCommand's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+            OleMenuCommandService commandService = await package.GetServiceAsync((typeof(IMenuCommandService))) as OleMenuCommandService;
+            Instance = new IISProcessAttachCommand(package, commandService);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            var iisProcess = new IISProcessAttach.Model.IISProcess(_package);
+            iisProcess.Attach();
+        }
+    }
+}
---
+++ IISProcessAttach/Model/IISProcess.cs
@@ -0,0 +1,43 @@
+﻿using EnvDTE;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System.Linq;
+
+namespace IISProcessAttach.Model
+{
+    public class IISProcess
+    {
+        private readonly string _iisProcessName = "w3wp.exe";
+        private readonly AsyncPackage _package;
+
+        public IISProcess(AsyncPackage package)
+        {
+            _package = package;
+        }
+
+        public void Attach()
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            var dte = (DTE)Package.GetGlobalService(typeof(DTE));
+            var processes = dte.Debugger.LocalProcesses;
+            var iisProcessList = processes.Cast<Process>().Where(proc => proc.Name.IndexOf(_iisProcessName) != -1).ToList();
+            if (iisProcessList.Any())
+            {
+                foreach (var iisProcess in iisProcessList)
+                {
+                    iisProcess.Attach();
+                }
+            }
+            else
+            {
+                VsShellUtilities.ShowMessageBox(
+                    _package,
+                    "No IIS process found          ",
+                    "",
+                    OLEMSGICON.OLEMSGICON_INFO,
+                    OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                    OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+            }
+        }
+    }
+}
